07df0fb1351b03fb0258b1567117e5b71273636f
OlivierArgentieri <olivierargentieri@gmail.com>
2020-11-03 10:25:13 +0100

    ok multiRootObjects open.py

--- Alembic/open.py
+++ Alembic/open.py
@@ -6,20 +6,26 @@ print(sys.argv)
 
 
 # from parameters
-fileName = sys.argv[4]
+mayaSceneFile = sys.argv[3]
+exportFileName = sys.argv[4]
 frameStart = sys.argv[5]
 frameEnd = sys.argv[6]
-objectName = sys.argv[7]
+rootObjects = sys.argv[7:]
 
 
 # open file
-cmds.file("D:\\Projet\\PullGithub\\Python-in-DCC\\Test\\Maya\\animation_sample.mb", open = True, force=True)
-print cmds.ls('pCube1')
+cmds.file(mayaSceneFile, open = True, force=True)
+
+# test object exist
 
 #command = "-frameRange " + "0" + " " + "20" +" -uvWrite -dataFormat ogawa -root " + "pCube1" + " -file " + "C:\\Users\\olivi\\Desktop\\test2.abc"
-# fram range -frameRange -0.25 -frameRelativeSample 0 -frameRelativeSample 0.5
-command = "-frameRange " + frameStart + " " + frameEnd +" -uvWrite -dataFormat ogawa -root " + objectName + " -file " + "C:\\Users\\olivi\\Desktop\\test2.abc"
-cmds.AbcExport ( j = command )
+
+for rootObject in rootObjects:
+    print rootObject 
+    # fram range -frameRange -0.25 -frameRelativeSample 0 -frameRelativeSample 0.5
+    command = "-frameRange " + frameStart + " " + frameEnd +" -uvWrite -dataFormat ogawa -root " + rootObject + " -file " + exportFileName +"_"+rootObject+".abc"
+    cmds.AbcExport ( j = command )
+
 # mayabatch. exe
 # maya-batch
 #./mayabatch.exe -script D:\Projet\PullGithub\Python-in-DCC\Alembic\test.mel
