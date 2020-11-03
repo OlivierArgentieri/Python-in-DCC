f3b7be832af469f4a19a7036666831fd0df73196
OlivierArgentieri <olivierargentieri@gmail.com>
2020-11-03 09:56:31 +0100

    add tes sample scene

--- Alembic/open.py
+++ Alembic/open.py
@@ -1,23 +1,28 @@
-import sys
+import sys, time
 import maya.cmds as cmds
+
 
 print(sys.argv)
 
 
 # from parameters
-# frameStart = sys.argv[0]
-# frameEnd = sys.argv[1]
-# objectName = sys.argv[2]
-# fileName = sys.argv[3]
+fileName = sys.argv[4]
+frameStart = sys.argv[5]
+frameEnd = sys.argv[6]
+objectName = sys.argv[7]
+
 
 # open file
-cmds.file("D:\\Projet\\PullGithub\\Artfx_Courses\\Haussman\\Micro_film\\Maya\\scenes\\Layout\\s010_animation_v007_final.mb", open = True, force=True)
+cmds.file("D:\\Projet\\PullGithub\\Python-in-DCC\\Test\\Maya\\animation_sample.mb", open = True, force=True)
+print cmds.ls('pCube1')
 
-command = "-frameRange " + "0" + " " + "10" +" -uvWrite -dataFormat ogawa -root " + "car02:root" + " -file " + "C:\\Users\\olivi\\Desktop\\test.abc"
+#command = "-frameRange " + "0" + " " + "20" +" -uvWrite -dataFormat ogawa -root " + "pCube1" + " -file " + "C:\\Users\\olivi\\Desktop\\test2.abc"
+# fram range -frameRange -0.25 -frameRelativeSample 0 -frameRelativeSample 0.5
+command = "-frameRange " + frameStart + " " + frameEnd +" -uvWrite -dataFormat ogawa -root " + objectName + " -file " + "C:\\Users\\olivi\\Desktop\\test2.abc"
 cmds.AbcExport ( j = command )
 # mayabatch. exe
 # maya-batch
 #./mayabatch.exe -script D:\Projet\PullGithub\Python-in-DCC\Alembic\test.mel
 #./mayabatch.exe -command 'python("execfile(\'D:/Projet/PullGithub/Python-in-DCC/Alembic/open.py\')");'
-# ./mayabatch.exe -script D:\Projet\PullGithub\Python-in-DCC\Alembic\test.mel "D:\\Projet\\PullGithub\\Artfx_Courses\\Haussman\\Micro_film\\Maya\\scenes\\Layout\\s010_animation_v007.mb"
+#./mayabatch.exe -script D:\Projet\PullGithub\Python-in-DCC\Alembic\test.mel "D:\\Projet\\PullGithub\\Python-in-DCC\\Test\\Maya\\animation_sample.mb"
 # #mayabatch.exe -batch -file someMayaFile.mb -command "file -save"
