aa146e7648e2cd8406afc1966694742fcf90071c
OlivierArgentieri <olivierargentieri@gmail.com>
2020-11-05 13:09:49 +0100

    ok script load alembic houdini, need to input fodler

--- Alembic/houdini/test.py
+++ Alembic/houdini/test.py
@@ -1,6 +1,25 @@
 import hou
+import os
+import sys #for argv
 
-#hou.hipFile.load('D:/Projet/PullGithub/Python-in-DCC/Alembic/houdini/test.hip')
-hou.hipFile.load('D:/awaaad.hip')
-hou.hipFile.save('D:/Projet/PullGithub/Python-in-DCC/Alembic/houdini/test.hip')
-#hou.hipFile.save('D:/awaaad3.hip')
+hou.hipFile.load('D:/Projet/PullGithub/Python-in-DCC/Alembic/houdini/houdiniSample/BaseTemplateHoudini.hip')
+
+hou.hipFile.save('D:/Projet/PullGithub/Python-in-DCC/Alembic/houdini/out/ImportAlembicTest.hip')
+hou.hipFile.load('D:/Projet/PullGithub/Python-in-DCC/Alembic/houdini/out/ImportAlembicTest.hip')
+
+# create base node
+
+obj = hou.node("/obj")
+
+obj.createNode("geo")
+
+obj = hou.node("/obj/geo1")
+obj.createNode("alembic")
+
+# create Alembic node
+obj = hou.node("/obj/geo1/alembic1")
+obj.parm('fileName').set('D:/Projet/PullGithub/Python-in-DCC/Test/Maya/pCube1.abc')
+
+# final save
+hou.hipFile.save('D:/Projet/PullGithub/Python-in-DCC/Alembic/houdini/out/ImportAlembicTest.hip')
+
