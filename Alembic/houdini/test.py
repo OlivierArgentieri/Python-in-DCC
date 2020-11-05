80de504332810d56324348dc8a3e26557115d08d
OlivierArgentieri <olivierargentieri@gmail.com>
2020-11-05 10:10:18 +0100

    add scripts python test save houdini

---
+++ Alembic/houdini/test.py
@@ -0,0 +1,6 @@
+import hou
+
+#hou.hipFile.load('D:/Projet/PullGithub/Python-in-DCC/Alembic/houdini/test.hip')
+hou.hipFile.load('D:/awaaad.hip')
+hou.hipFile.save('D:/Projet/PullGithub/Python-in-DCC/Alembic/houdini/test.hip')
+#hou.hipFile.save('D:/awaaad3.hip')
